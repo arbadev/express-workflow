13255bf1f274b38cfa609f27eec6cd3515fe4d59
Andres Barradas <arbadev@gmail.com>
2016-09-17 18:03:17 -0400

    db integration, and hits CRUD

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+/*
+* Mongodb
+*
+*/
+
+mongoose.connect('mongodb://localhost/test');
+
+/*
+*    Routes import
+*
+*/
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var hits = require('./routes/hits');
 
 var app = express();
+
+/*
+*    Routes at app
+*
+*/
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/hits', hits);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +45,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/hits.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var hitController = require('../controllers/hitController.js');
+var bodyParser = require('body-parser')
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+/*
+ * GET
+ */
+router.get('/', function (req, res) {
+  hitController.list(req, res);
+});
+
+/*
+ * GET
+ */
+router.get('/:id', function (req, res) {
+    hitController.show(req, res);
+});
+
+/*
+ * POST
+ */
+router.post('/', jsonParser, function (req, res) {
+    hitController.create(req, res);
+});
+
+/*
+ * PUT
+ */
+router.put('/:id', function (req, res) {
+    hitController.update(req, res);
+});
+
+/*
+ * DELETE
+ */
+router.delete('/:id', function (req, res) {
+    hitController.remove(req, res);
+});
+
+module.exports = router;
